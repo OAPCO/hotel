70b8a8874f9c654790f509f7b4f0a2cc64c2baa6
woori <rntmwjstk96@gmail.com>
2024-04-02 11:17:02 +0900

    first commit

---
+++ src/main/java/com/exam/hotelgers/HotelgersApplication.java
@@ -0,0 +1,13 @@
+package com.exam.hotelgers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelgersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelgersApplication.class, args);
+	}
+
+}
