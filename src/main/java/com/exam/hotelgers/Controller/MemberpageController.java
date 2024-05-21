9a31eb539a76dde8683d1fb9b6f11b682cecde8d
woori <rntmwjstk96@gmail.com>
2024-05-21 10:37:24 +0900

    1.footer 공지사항 링크 2.카카오톡 이름도 받게 3.admin 회원id 이메일로

--- src/main/java/com/exam/hotelgers/Controller/MemberpageController.java
+++ src/main/java/com/exam/hotelgers/Controller/MemberpageController.java
@@ -464,7 +464,16 @@ public class MemberpageController {
         return "member/memberpage/qnacenter";
     }
 
+    @GetMapping("/member/memberpage/announcement")
+    public String announcementForm(@PageableDefault(page = 1) Pageable pageable, Model model) {
+        Page<NoticeDTO> noticeDTOS = noticeService.list(pageable);
 
+        Map<String, Integer> pageinfo = PageConvert.Pagination(noticeDTOS);
+
+        model.addAllAttributes(pageinfo);
+        model.addAttribute("list", noticeDTOS);
+        return "member/memberpage/announcement";
+    }
 
 
     @GetMapping("/member/mypage/point")
